fc6abfe3d28f4a5420ef63b4f0345219f922a203
persona330 <61354096+persona330@users.noreply.github.com>
2022-04-28 20:12:10 +0500

    Сохранение Customer

---
+++ src/main/java/com/example/retail/model/entity/Customer.java
@@ -0,0 +1,38 @@
+package com.example.retail.model.entity;
+
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+/**
+ * Класс Покупатель со свойствами <b>id_Customer<b/>
+ */
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "CUSTOMER")
+public class Customer
+{
+    /** Свойство идентификатор покупателя*/
+    private Integer id_Customer;
+
+    /**
+     * Конструктор - создание нового объекта с определенными значениями
+     * @param id_Customer
+     * @see Customer#Customer()
+     */
+    public Customer(Integer id_Customer)
+    {
+        this.id_Customer = id_Customer;
+    }
+    /**
+     * Конструктор - создание нового объекта
+     * @see Customer#Customer(Integer)
+     */
+    public Customer(){}
+
+    @Id
+    @GeneratedValue(generator = "SQLCustomer", strategy = GenerationType.AUTO)
+    @Column(name = "ID_Customer", unique = true, nullable = false)
+    public Integer getId_Customer() { return id_Customer; }
+    public void setId_Customer(Integer id_Customer) { this.id_Customer = id_Customer; }
+}
