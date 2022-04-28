7b6234e9dd60306b78eeda647145852df969bde4
persona330 <61354096+persona330@users.noreply.github.com>
2022-04-28 21:52:56 +0500

    Сохранение Producer

---
+++ src/main/java/com/example/retail/model/entity/Producer.java
@@ -0,0 +1,12 @@
+package com.example.retail.model.entity;
+
+/**
+ * Класс Производитель со свойствами <b>id_produkt<b/>, <b>voll_name<b/>, <b>menge_auf_lager<b/>
+ */
+public class Producer
+{
+    /** Свойство идентификатор производителя товаров*/
+    private Integer id_Producer;
+    /**Свойство наименование компании производителя*/
+    private String name;
+}
