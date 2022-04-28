d8ebcfb0ae3761c3c54c35627f7c13c706a66350
persona330 <61354096+persona330@users.noreply.github.com>
2022-04-28 19:11:36 +0500

    Сохранение Address

---
+++ src/main/java/com/example/retail/model/entity/Communication.java
@@ -0,0 +1,14 @@
+package com.example.retail.model.entity;
+
+/**
+ * Класс СредствоСвязи со свойствами <b>id_Address<b/>, <b>apartment<b/>, <b>entrance<b/>
+ */
+public class Communication
+{
+    /** Свойство идентификатор средства связи*/
+    private int id_Communication;
+    /**Свойство номер телефона*/
+    private char phone;
+    /**Свойство название электронного адреса*/
+    private char email;
+}
