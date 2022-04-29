9b9485eb017d1695c0bf439a2c3a4211f37bda30
persona330 <61354096+persona330@users.noreply.github.com>
2022-04-29 19:37:24 +0500

    Сохранение

---
+++ src/main/java/com/example/retail/model/entity/Position.java
@@ -0,0 +1,12 @@
+package com.example.retail.model.entity;
+
+/**
+ * Класс Должность со свойствами <b>id_produkt<b/>, <b>voll_name<b/>, <b>menge_auf_lager<b/>
+ */
+public class Position extends BaseEntity
+{
+    /** Свойство идентификатор должности работника*/
+    private int id_Position;
+    /**Свойство наименование должности*/
+    private String name;
+}
